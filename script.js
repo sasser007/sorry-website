2b39ff71d5a4433519248f4f238d004d58e58771
sasser007 <ridhir170@gmail.com>
2026-01-29 19:29:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const noBtn = document.getElementById("no");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+function yes() {
+  window.location.href = "gifts.html";
+}
